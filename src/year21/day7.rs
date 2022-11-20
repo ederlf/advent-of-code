d497edd6363e314285b2f233eec07b9f0876ddc2
Eder Leão Moosmann <eder.leaofernandes@platform.sh>
2022-11-20 14:05:37 +0100

    2021, day 07

--- src/year21/day7.rs
+++ src/year21/day7.rs
@@ -1,9 +1,33 @@
-fn part1(_input: String)  -> String {
-    "Not implemented".to_string()
+
+
+fn part1(input: String)  -> String {
+    let positions: Vec<usize> = input.trim_end_matches(&['\r', '\n'][..])
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap()).collect();
+    let mut min_fuel = usize::MAX;
+    for pos in positions.iter(){
+        let fuel = positions.iter().fold(0, |acc, x| acc + x.abs_diff(*pos));
+        min_fuel = if fuel < min_fuel {fuel} else {min_fuel};
+
+    }
+    min_fuel.to_string()
 }
 
-fn part2(_input: String) -> String {
-    "Not implemented".to_string()
+fn natural_sum(v: usize) -> usize {
+    (v * (v + 1)) / 2
+}
+
+fn part2(input: String) -> String {
+    let positions: Vec<usize> = input.trim_end_matches(&['\r', '\n'][..])
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap()).collect();
+    let mut min_fuel = usize::MAX;
+    for pos in 0..*positions.iter().max().unwrap(){
+        let fuel = positions.iter().fold(0, |acc, x| acc + natural_sum(x.abs_diff(pos)) )  ;
+        min_fuel = if fuel < min_fuel {fuel} else {min_fuel};
+
+    }
+    min_fuel.to_string()
 }
 
 pub fn solve(input: String) -> (String, String) {
