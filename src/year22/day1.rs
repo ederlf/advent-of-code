77f262b1e78dc16d052623284de1ba993d9bd1c7
Eder Leão Moosmann <eder.leaofernandes@platform.sh>
2022-12-01 09:47:57 +0100

    Year 2022, day 01

--- src/year22/day1.rs
+++ src/year22/day1.rs
@@ -1,9 +1,26 @@
-fn part1(_input: String)  -> String {
-    "Not implemented".to_string()
+fn parse_elves(input: String) -> Vec<usize> {
+    input.lines().fold(Vec::new(), |mut acc, x| {
+            if x == "" || acc.is_empty() {
+                acc.push(Vec::new());
+                return acc
+            }
+            acc.last_mut().unwrap().push(x.parse::<usize>().unwrap());
+            acc
+        }).iter()
+        .map(|x| x.iter().sum::<usize>()).collect()
 }
 
-fn part2(_input: String) -> String {
-    "Not implemented".to_string()
+fn part1(input: String)  -> String {
+    let elves = parse_elves(input);
+    let max: usize  = *elves.iter().max().unwrap();
+    max.to_string()
+}
+
+fn part2(input: String) -> String {
+    let mut elves = parse_elves(input);
+    elves.sort_by(|a, b| b.cmp(a));
+    let total = elves.iter().take(3).sum::<usize>();
+    total.to_string()
 }
 
 pub fn solve(input: String) -> (String, String) {
