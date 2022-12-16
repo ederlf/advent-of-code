436527e0021cf700ef07ed71c3a46ad58a9cfe27
Eder Leao Moosmann <ederleaofernandes@gmail.com>
2022-12-17 00:31:52 +0100

    Year 2022, day 13 part 2

--- src/year22/day13.rs
+++ src/year22/day13.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use std::cmp::Ordering;
+
+#[derive(Debug, Clone)]
 enum Position {
     Number(u32),
     List(Vec<Position>),
@@ -39,16 +41,16 @@ fn parse_packet(packet: &str) -> Vec<Position> {
     p
 }
 
-fn compare_packets(p1: &Vec<Position>, p2: &Vec<Position>) -> (bool, bool) {
+fn compare_packets(p1: &Vec<Position>, p2: &Vec<Position>) -> (bool, bool, Ordering) {
     for (x, y) in p1.iter().zip(p2) {
-        let (cont, res) = match (x, y) {
+        let (cont, res, ordering) = match (x, y) {
             (Position::Number(v1), Position::Number(v2)) => {
                 if *v1 > *v2 {
-                    return (false, false);
+                    return (false, false, Ordering::Greater);
                 } else if *v1 < *v2 {
-                    return (false, true);
+                    return (false, true, Ordering::Less);
                 }
-                (true, true)
+                (true, true, Ordering::Less)
             }
             (Position::List(v1), Position::List(v2)) => compare_packets(v1, v2),
             (Position::List(v1), Position::Number(v2)) => {
@@ -64,21 +66,21 @@ fn compare_packets(p1: &Vec<Position>, p2: &Vec<Position>) -> (bool, bool) {
         };
 
         if !cont {
-            return (false, res);
+            return (false, res, ordering);
         }
     }
 
     if p1.is_empty() {
-        return (false, true);
+        return (false, true, Ordering::Less);
     }
 
     // if all the comparisons on matching positions are the same
     // we get here. If p1 is larger than p2 it means p2 does
     // ran out of elements
     if p1.len() > p2.len() {
-        return (false, false);
+        return (false, false, Ordering::Greater);
     }
-    (true, true)
+    (true, true, Ordering::Less)
 }
 
 fn part1(input: String) -> String {
@@ -96,8 +98,50 @@ fn part1(input: String) -> String {
     ordered.to_string()
 }
 
-fn part2(_input: String) -> String {
-    "Not implemented".to_string()
+fn part2(input: String) -> String {
+    let ext = format!("{}\n{}", input, "[[2]]\n[[6]]");
+    let mut packets: Vec<Vec<Position>> = ext
+        .lines()
+        .filter(|x| !x.is_empty())
+        .map(|x| parse_packet(x))
+        .collect();
+    packets.sort_by(|a, b| compare_packets(a, b).2);
+    // Improve this, oh the uglyness
+    let v = packets.iter().enumerate().fold(1, |acc, (i, x)| {
+        acc * {
+            if x.len() == 1 {
+                let mut idx = 1;
+                for packet in x {
+                    let list = match packet {
+                        Position::List(v) => v.clone(),
+                        _ => {
+                            let v: Vec<Position> = Vec::new();
+                            v
+                        }
+                    };
+
+                    if list.len() != 1 {
+                        continue;
+                    }
+
+                    idx = match list[0] {
+                        Position::Number(n) => {
+                            if n == 2 || n == 6 {
+                                i + 1
+                            } else {
+                                1
+                            }
+                        }
+                        _ => 1,
+                    };
+                }
+                idx
+            } else {
+                1
+            }
+        }
+    });
+    v.to_string()
 }
 
 pub fn solve(input: String) -> (String, String) {
