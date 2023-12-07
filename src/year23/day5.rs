b13a460e4bc3ef6868faaac8fb0dd9bd223fa569
Eder Leao Moosmann <ederleaofernandes@gmail.com>
2023-12-07 21:29:16 +0100

    Year 2023, day 05 part 2

--- src/year23/day5.rs
+++ src/year23/day5.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 #[derive(Debug)]
 struct Map {
     destination: usize,
@@ -32,6 +30,16 @@ fn map_to_destination(source: usize, maps: &Vec<Map>) -> usize {
         }
     }
     source
+}
+
+fn map_destination_to_source(destination: usize, maps: &Vec<Map>) -> usize {
+    for map in maps {
+        if destination >= map.destination && destination < map.destination + map.range {
+            let offset = destination - map.destination;
+            return map.source + offset;
+        }
+    }
+    destination
 }
 
 fn part1(input: String) -> String {
@@ -68,75 +76,48 @@ fn part1(input: String) -> String {
     locations.iter().min().unwrap().to_string()
 }
 
-fn map_source_range_to_destination_range(
-    source_start: usize,
-    source_end: usize,
-    maps: &Vec<Map>,
-) -> HashSet<(usize, usize)> {
-    let mut ranges = HashSet::new();
-    println!("{} {}", source_start, source_end);
-    for map in maps {
-        let mut start_destination = source_start;
-        let mut end_destination = source_end;
-        if source_start >= map.source && source_start <= map.source + map.range {
-            let offset = source_start - map.source;
-            start_destination = map.destination + offset;
-        }
-        if source_end >= map.source && source_end <= map.source + map.range {
-            let offset = source_end - map.source;
-            end_destination = map.destination + offset;
-        }
-        ranges.insert((start_destination, end_destination));
-    }
-    ranges
-}
-
 fn part2(input: String) -> String {
-    //let mut maps = input.split("\n\n");
-    //let seeds: Vec<usize> = maps
-    //    .next()
-    //    .unwrap()
-    //    .split(':')
-    //    .nth(1)
-    //    .unwrap()
-    //    .trim()
-    //    .split(' ')
-    //    .map(|x| x.parse::<usize>().unwrap())
-    //    .collect();
+    let mut maps = input.split("\n\n");
+    let seeds: Vec<usize> = maps
+        .next()
+        .unwrap()
+        .split(':')
+        .nth(1)
+        .unwrap()
+        .trim()
+        .split(' ')
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
 
-    //let seed_to_soil = parse_map(maps.next().unwrap());
-    //let soil_to_fertilizer = parse_map(maps.next().unwrap());
-    //let fertilize_to_water = parse_map(maps.next().unwrap());
-    //let water_to_light = parse_map(maps.next().unwrap());
-    //let light_to_temperature = parse_map(maps.next().unwrap());
-    //let temperature_to_humidity = parse_map(maps.next().unwrap());
-    //let humidity_to_location = parse_map(maps.next().unwrap());
+    let seed_to_soil = parse_map(maps.next().unwrap());
+    let soil_to_fertilizer = parse_map(maps.next().unwrap());
+    let fertilize_to_water = parse_map(maps.next().unwrap());
+    let water_to_light = parse_map(maps.next().unwrap());
+    let light_to_temperature = parse_map(maps.next().unwrap());
+    let temperature_to_humidity = parse_map(maps.next().unwrap());
+    let humidity_to_location = parse_map(maps.next().unwrap());
 
-    //for seeds_range in seeds.chunks(2) {
-    //    let end_seeds = seeds_range[0] + seeds_range[1];
-    //    let soil = map_source_range_to_destination_range(seeds_range[0], end_seeds, &seed_to_soil);
-    //    let fertilizer = map_source_range_to_destination_range(soil.0, soil.1, &soil_to_fertilizer);
-    //    let water =
-    //        map_source_range_to_destination_range(fertilizer.0, fertilizer.1, &fertilize_to_water);
-    //    let light = map_source_range_to_destination_range(water.0, water.1, &water_to_light);
-    //    let temperature =
-    //        map_source_range_to_destination_range(light.0, water.1, &light_to_temperature);
-    //    let humidity = map_source_range_to_destination_range(
-    //        temperature.0,
-    //        temperature.1,
-    //        &temperature_to_humidity,
-    //    );
-    //    let location =
-    //        map_source_range_to_destination_range(humidity.0, humidity.1, &humidity_to_location);
-    //    //locations.insert(seed, location);
-    //    println!(
-    //        "{:?} {:?} {:?} {:?} {:?} {:?} {:?} {:?}",
-    //        seeds_range, soil, fertilizer, water, light, temperature, humidity, location
-    //    );
-    //}
-    //"".to_string()
-    ////locations.values().min().unwrap().to_string()
-    "".to_string()
+    let mut ranges = Vec::new();
+    for seeds_range in seeds.chunks(2) {
+        ranges.push(seeds_range[0]..seeds_range[0] + seeds_range[1]);
+    }
+    let mut location = 0;
+    'outer: loop {
+        location += 1;
+        let humidity = map_destination_to_source(location, &humidity_to_location);
+        let temperature = map_destination_to_source(humidity, &temperature_to_humidity);
+        let light = map_destination_to_source(temperature, &light_to_temperature);
+        let water = map_destination_to_source(light, &water_to_light);
+        let fertilizer = map_destination_to_source(water, &fertilize_to_water);
+        let soil = map_destination_to_source(fertilizer, &soil_to_fertilizer);
+        let seed = map_destination_to_source(soil, &seed_to_soil);
+        for range in &ranges {
+            if range.contains(&seed) {
+                break 'outer;
+            }
+        }
+    }
+    location.to_string()
 }
 
 pub fn solve(input: String) -> (String, String) {
