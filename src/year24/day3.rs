7d8cdc5cefcc667cfdfc28fb249f4024d2b03b46
Eder Leao Moosmann <elm@mail.ederlm.de>
2024-12-06 21:38:03 +0100

    Day 3, 2024

--- src/year24/day3.rs
+++ src/year24/day3.rs
@@ -1,9 +1,64 @@
-fn part1(_input: String)  -> String {
-    "Not implemented".to_string()
+use regex::Regex;
+
+fn part1(input: String)  -> String {
+    let pattern = r"mul\((\d+),(\d+)\)";
+    let re = Regex::new(pattern).unwrap();
+
+
+    let mut total_sum: u64 = 0;
+
+    for cap in re.captures_iter(&input) {
+        let num1: u64 = cap[1].parse().unwrap();
+        let num2: u64 = cap[2].parse().unwrap();
+        let product = num1 * num2;
+        total_sum += product;
+    }
+
+    total_sum.to_string()
 }
 
-fn part2(_input: String) -> String {
-    "Not implemented".to_string()
+fn part2(input: String) -> String {
+    let pattern = r"(don't\(\)|do\(\))";
+    let re = Regex::new(pattern).unwrap();
+    let mul_pattern = r"mul\((\d+),(\d+)\)";
+    let mul_re = Regex::new(mul_pattern).unwrap();
+
+    let mut last_index = 0;
+    let mut total_sum: u64 = 0;
+
+    let mut mul = true;
+    for mat in re.find_iter(&input) {
+        // Get the text before the current delimiter
+        let before_delimiter = &input[last_index..mat.start()];
+        if mul {
+            for cap in mul_re.captures_iter(&before_delimiter) {
+                let num1: u64 = cap[1].parse().unwrap();
+                let num2: u64 = cap[2].parse().unwrap();
+                let product = num1 * num2;
+                total_sum += product;
+            }
+        }
+        if mat.as_str().contains("don't") {
+            mul = false;
+        } else {
+            mul = true;
+        }
+
+        last_index = mat.end();
+    }
+
+    if last_index < input.len() {
+        if mul {
+            for cap in mul_re.captures_iter(&input[last_index..]) {
+                let num1: u64 = cap[1].parse().unwrap();
+                let num2: u64 = cap[2].parse().unwrap();
+                let product = num1 * num2;
+                total_sum += product;
+            }
+        }
+    }
+
+    total_sum.to_string()
 }
 
 pub fn solve(input: String) -> (String, String) {
